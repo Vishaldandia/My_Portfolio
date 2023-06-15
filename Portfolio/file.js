eaa5eac71e7b8209cb4e68b02c50a050ce394788
Vishaldandia <108942533+Vishaldandia@users.noreply.github.com>
2023-06-15 22:43:58 +0530

    Add files via upload

---
+++ Portfolio/file.js
@@ -0,0 +1,13 @@
+// Smooth scroll to section on navbar link click
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    
+    const target = document.querySelector(this.getAttribute('href'));
+    
+    window.scrollTo({
+      top: target.offsetTop,
+      behavior: 'smooth'
+    });
+  });
+});
